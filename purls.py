6731ca6d96b17b614fd02d273bc54c7df83150af
Jesse Jaggars <jjaggars@redhat.com>
2017-05-03 16:06:26 -0500

    a slightly hacky yet better way

--- purls.py
+++ purls.py
@@ -3,6 +3,11 @@
 import re
 import sys
 
-blob = re.sub(r"\n(\S)", r"\1", sys.stdin.read())
-urls = re.findall(r"(https?://[A-Za-z0-9-._~:/?#\[\]@!$&'()*+,;=%]+)", blob)
-print "\n".join(urls)
+pat = re.compile(r"(https?://[A-Za-z0-9-._~:/?#\[\]@!$&'()*+,;=%]+)")
+blob = "\n".join([line.strip() for line in sys.stdin.readlines()])
+
+smashed = re.sub(r"\n(\S)", r"\1", blob)
+urls = pat.findall(blob)
+urls += pat.findall(smashed)
+
+print "\n".join(sorted(list(set(urls))))
