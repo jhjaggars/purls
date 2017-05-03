30aaa96fc7a5f3875a56001c8d3a9d96071bc736
Jesse Jaggars <jjaggars@redhat.com>
2017-05-03 13:10:26 -0500

    attempt to handle links broken over multiple lines

--- purls.py
+++ purls.py
@@ -3,6 +3,6 @@
 import re
 import sys
 
-blob = sys.stdin.read()
-urls = re.findall(r"(https?://[A-Za-z0-9-._~:/?#\[\]@!$&'()*+,;=]+)", blob)
+blob = re.sub(r"\n(\S)", r"\1", sys.stdin.read())
+urls = re.findall(r"(https?://[A-Za-z0-9-._~:/?#\[\]@!$&'()*+,;=%]+)", blob)
 print "\n".join(urls)
